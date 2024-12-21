96d8ce288afc5dbd2ef8812209942fc2c2e0cee2
SaharTheGreat <SarahECartwright96@gmail.com>
2024-12-20 22:28:49 -0700

    added FPS limit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while 1==1:
         for event in pygame.event.get():
@@ -20,6 +22,8 @@ def main():
 
 
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+
 
 
 
